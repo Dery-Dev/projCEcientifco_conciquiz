93aa9b7dda8a0fe5c965751685feaa8b89fb9862
yuki-gamerOS <yugameryurigamer@gmail.com>
2026-08-16 21:38:03 -0300

    atualizando de vdd agora

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-const botaoPuxar = document.getElementById("puxar");
-const botaoNaoPuxar = document.getElementById("nao-puxar");
-
-botaoPuxar.addEventListener("click", function() {
-    console.log("O jogador puxou a alavanca!");
-});
-
-botaoNaoPuxar.addEventListener("click", function() {
-    console.log("O jogador não puxou a alavanca!");
-});
-
+// Seleção dos elementos da tela inicial e botões
 const telaInicial = document.getElementById("telaInicial");
 const botaoComecar = document.getElementById("botaoComecar");
 const botoes = document.getElementById("botoes");
+
+// Seleção dos elementos do cenário do jogo
+const trilhos = document.querySelector(".trilhos");
+const trem = document.querySelector(".trem");
+const bonecoAlavanca = document.querySelector(".boneco-alavanca");
 
 botaoComecar.addEventListener("click", function() {
 
     telaInicial.style.display = "none";
 
+// 2. Exibe os elementos do jogo
+    trilhos.style.display = "block";
+    trem.style.display = "block";
+    bonecoAlavanca.style.display = "block";
+    
     botoes.classList.add("visivel");
+}); 
 
-
-});
+var botaoPuxar = document.getElementById("puxar");
+var botaoNaoPuxar = document.getElementById("nao-puxar");
+function continuar() { 
+}
