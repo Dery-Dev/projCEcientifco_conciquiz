d66ae50529be195119f4842eb99538018a1155e3
yuki-gamerOS <yugameryurigamer@gmail.com>
2026-08-23 20:09:57 -0300

    fiz em partes a tela final e pequenas alteções no resto

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 const telaInicial = document.getElementById("telaInicial");
 const botaoComecar = document.getElementById("botaoComecar");
 
+//tela final
+const telafinal1 = document.getElementById('Telafinal-b')
+const telafinal2 = document.getElementById('Telafinal-n')
+const telafinal3 = document.getElementById('Telafinal-r')
+
+telafinal1.style.display = "none"
+telafinal2.style.display = "none"
+telafinal3.style.display = "none"
+
 // Botões de decisão
 const botoes = document.getElementById("botoes");
 const botaoPuxar = document.getElementById("puxar");
@@ -20,18 +29,112 @@ const trilhos = document.querySelector(".trilhos");
 const trem = document.querySelector(".trem");
 const bonecoAlavanca = document.querySelector(".boneco-alavanca");
 const bonecoAlavanca1 = document.querySelector(".boneco-alavanca1");
+// PESSOAS
+
+const cincoPessoas = document.getElementById("cincoPessoas");
+const umaPessoa = document.getElementById("umaPessoa");
+const pessoaRica = document.getElementById("pessoaRica");
+const monalisa = document.getElementById("monalisa");
+const cincodormindo = document.getElementById("cincodormindo")
+const idosos = document.getElementById("idosos")
+const enemy = document.getElementById("enemy")
+const cat = document.getElementById("cat")
+const politic = document.getElementById("politic")
+
+function esconderPessoas() {
+    cincoPessoas.style.display = "none";
+    umaPessoa.style.display = "none";
+    pessoaRica.style.display = "none";
+    monalisa.style.display = "none";
+    cincodormindo.style.display = "none";
+    idosos.style.display = "none";
+    enemy.style.display = "none";
+    cat.style.display = "none";
+    politic.style.display = "none";
+}
+        esconderPessoas();
+
+    // MOSTRAR AS PESSOAS DE ACORDO COM A PERGUNTA
+function mostrarPessoas() {
+
+    esconderPessoas();
+
+    // PERGUNTA 1
+    if (perguntaAtual === 0) {
+
+        cincoPessoas.style.display = "block";
+        umaPessoa.style.display = "block";
+
+    }
+
+    // PERGUNTA 2
+    else if (perguntaAtual === 1) {
+
+        cincodormindo.style.display = "block";
+        umaPessoa.style.display = "block";
+
+    }
+
+    // PERGUNTA 3
+    else if (perguntaAtual === 2) {
+
+        idosos.style.display = "block";
+        umaPessoa.style.display = "block";
+
+    }
+
+    // PERGUNTA 4
+    else if (perguntaAtual === 3) {
+
+        politic.style.display = "block";
+        umaPessoa.style.display = "block";
+
+    }
+
+    // PERGUNTA 5
+    else if (perguntaAtual === 4) {
+
+        idosos.style.display = "block";
+        cat.style.display = "block";
+        umaPessoa.style.display = "block";
+
+    }
+
+    // PERGUNTA 6
+    else if (perguntaAtual === 5) {
+
+        politic.style.display = "block";
+        monalisa.style.display = "block";
+
+    }
+
+    // PERGUNTA 7
+    else if (perguntaAtual === 6) {
+
+        enemy.style.display = "block";
+        pessoaRica.style.display = "block";
+
+    }
+}
 
 
 // PERGUNTAS
 
 const perguntas = [
-    "Pergunta de teste 1",
-    "Pergunta de teste 2",
-    "O trem vai passar em cima de 5 idosos inocentes, se você puxar a alavanca, o trem passará por cima de um parente aleatório seu.",
-    "O trem passará por cima de 10 políticos corruptos, que se forem mortos, farão você ser caçado por todo o país se você puxar a alavanca, o trem matará um político honesto que governava uma cidade em ascensão",
-    "O trem vai passar por cima de uma caixa que possui uma chance de ter um animal aleatório lá dentro, se você puxar a alavanca, o trem passará por cima de um biólogo que acabou de descobrir uma nova espécie que ainda não foi catalogada.",
-    "O trem passará por cima de 5 pessoas com uma doença rara, se você puxar a alavanca,o trem passará por cima do único médico que pode curar pessoas com esse tipo de doença de graça. (elas não são as únicas no mundo com essa doença)",
-    "O trem passará por cima de 100 pessoas completamente inocentes, se você puxar a alavanca, o trem passará por cima da sua mãe e de seu pai."
+    "Um trem está indo em direção a 5 pessoas. Você pode puxar a alavanca para desvia-lo para o outro trilho, matando 1 pessoa, você puxa a alavanca?",
+
+    "O trem está indo em direção a 5 pessoas dormindo. Você pode puxar a alavanca para desvia-lo para o outro trilho, matando 1 pessoa inocente que tem uma família acordada, Você puxa a alavanca?",
+
+    "O trem vai passar em cima de 5 idosos que se viver terão apenas no maxímo 2 anos a mais de vida , se você puxar a alavanca, o trem passará por cima de um parente seu, podendo ser seu pai, tio ou avô.",
+
+    "O trem passará por cima de 1 político que rouba e roubou milhões de pessoas , que se for morto fará você que estava perto ser caçado por todo o país, se você puxar a alavanca, o trem matará uma pessoa completamente inocente vitíma desse político.",
+
+    "O trem vai passar por cima de cinco idosos que não viveram até o final do ano, se você puxar a alavanca, o trem passará por cima de um adoravel gatinho e seu dono que ainda irá viver 50 anos.",
+
+    "O trem passará por cima de 1 politico corrupto , se você puxar a alavanca,o trem passará por cima da monalisa original e que você pagara pela destruição dela.",
+
+    "O trem passará por cima do seu maior inimigo, o qual matou a pessoa que você mais amava num acidente, se você puxar a alavanca, o trem passará por cima de um homem rico que realiza praticas criminosas, inncluindo assassinato, mas te oferece 1 milhão se sobreviver."
+
 ];
 
 let perguntaAtual = 0;
@@ -62,6 +165,7 @@ botaoComecar.addEventListener("click", function() {
     botoes.classList.add("visivel");
 
     mostrarPergunta();
+    mostrarPessoas();
 
 });
 
@@ -69,9 +173,11 @@ function virar() {
     bonecoAlavanca.style.display = "none";
     bonecoAlavanca1.classList.add("visivel1")
     bonecoAlavanca1.style.display = "block";
+    if (perguntaAtual == 0 || perguntaAtual == 1 || perguntaAtual == 2 || perguntaAtual == 6) {
     pontos.style.display = "block"
     ponto++
     pontos.textContent = `Pontos de moral: ${ponto}`
+    }
 }
 
 function continuar() {
@@ -79,6 +185,11 @@ function continuar() {
     bonecoAlavanca.style.display = "block";
     bonecoAlavanca1.style.display = "none";
     bonecoAlavanca1.classList.remove("visivel1");
+    if (perguntaAtual == 3 || perguntaAtual == 5 || perguntaAtual == 4 ) {
+    pontos.style.display = "block"
+    ponto++
+    pontos.textContent = `Pontos de moral: ${ponto}`
+    }
 }
 
 // ESCOLHA: PUXAR
@@ -91,25 +202,8 @@ botaoPuxar.addEventListener("click", function() {
 // ESCOLHA: NÃO PUXAR
 
 botaoNaoPuxar.addEventListener("click", function() {
-    continuar()
     iniciarTurno();
 });
-
-
-// INICIAR TURNO
-
-function iniciarTurno() {
-
-    botoes.classList.remove("visivel");
-    // simula a animação do trem
-    setTimeout(function() {
-
-  containerNext.style.display = "flex";
-
-    }, 2000);
-
-}
-
 
 // BOTÃO NEXT
 const botaoNextElement = document.getElementById("next");
@@ -126,6 +220,8 @@ botaoNext.addEventListener("click", function() {
     if (perguntaAtual < perguntas.length) {
 
         mostrarPergunta();
+        mostrarPessoas();
+
 
         botoes.classList.add("visivel");
        bonecoAlavanca.style.display = "block" 
@@ -133,13 +229,42 @@ botaoNext.addEventListener("click", function() {
     else {
 
         // Chegou ao fim das 7 perguntas
-        console.log("Fim");
+        ;
         trilhos.style.display = "none"
         trem.style.display = "none"
         pergunta.style.display = "none"
         bonecoAlavanca.style.display = "none"
         bonecoAlavanca1.style.display = "none"
+        enemy.style.display = "none";
+        pessoaRica.style.display = "none";
+        if (ponto == 7) {
+        telafinal1.style.display = "block"
+        } else if (ponto < 7 && ponto > 3){ 
+            telafinal2.style.display = "block"; 
+        } else {
+            telafinal3.style.display = "block"
+        }
     }
 
 });
 
+// INICIAR TURNO
+
+function iniciarTurno() {
+
+    botoes.classList.remove("visivel");
+    // simula a animação do trem
+    setTimeout(function() {
+
+  containerNext.style.display = "flex";
+
+    }, 2000);
+
+}
+
+
+
+
+console.log(cincoPessoas);
+console.log(umaPessoa);
+console.log(politic);
